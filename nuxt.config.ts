e57ba842c748cc3725313b3dd842c07c2725f788
Mohamed Tammam <mr.mohamedabdallah98@gmail.com>
2022-12-02 17:38:31 +0200

    Add page title and description meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
   ssr: false,
+  app: {
+    head: {
+      title: 'Javascript Quiz Questions',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Javascript quiz generated from the most popular JavaScript questions repo.' }
+      ],
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ],
